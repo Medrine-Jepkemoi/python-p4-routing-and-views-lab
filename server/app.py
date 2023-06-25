e00c4f043707c0c09a5bfa702fb7ff392c5b8726
Medrine-Jepkemoi <jepkemoimedrine@gmail.com>
2023-06-25 19:22:49 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -13,22 +13,36 @@ def print_string(parameter):
     print (parameter)
     return parameter
 
+
+
+
 @app.route('/count/<int:number>')
 def count(number):
     numlst = []
-    i = 0
-    for i in range (number):
-        print(i)
-        numlst.append(i)
-        # i += 1
-    return numlst
+    for i in range(number):
+        numlst.append(str(i))
+    return '\n'.join(numlst)
 
-    # return number
-    # i = 0
-    # for i in range(len(parameter)):
-    #     print(i)
-    #     i += 1
 
+@app.route('/math/<int:num1><operation><int:num2>')
+def math(num1, operation, num2):
+    result = None
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+    elif operation == '%':
+        result = num1 % num2
+    
+    if result is not None:
+        return str(result)
+    else:
+        return 'Invalid operation or division by zero'
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
